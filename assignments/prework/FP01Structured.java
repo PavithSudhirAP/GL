d45e1c1d12ea0254993c676bef9914b550bcffba
Indhu Sudhakaran <62611460+Indhusudhakaran@users.noreply.github.com>
2022-10-28 16:04:05 +0530

    Update FP01Structured.java

--- assignments/prework/FP01Structured.java
+++ assignments/prework/FP01Structured.java
@@ -8,7 +8,7 @@ public class FP01Structured {
 	public static void main(String[] args) {
 		
 		printAllNumbersInListStructured(Arrays.asList(1,2));
-		//123
+		
 	}
 	
 	public static void printAllNumbersInListStructured(List<Integer> numbers) {
